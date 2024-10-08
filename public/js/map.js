d122c9ec43a7aa7943914b06a10639bd990e799a
Ankit kumawat <ankitkumawatankit70@gmail.com>
2024-10-08 18:42:17 +0530

    add map functionality

--- public/js/map.js
+++ public/js/map.js
@@ -1,15 +1,12 @@
-
 mapboxgl.accessToken = mapToken
 
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style:"mapbox://styles/mapbox/streets-v12" ,  //style url 
-    center: [75.7158, 27.1656], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    center:coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
     zoom: 9 // starting zoom
  });
 
  const marker = new mapboxgl.Marker()
-        .setLngLat([12.554729, 55.70651])
+        .setLngLat(coordinates)
         .addTo(map);
-
-     
